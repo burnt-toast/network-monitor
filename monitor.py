7f69d9fbf032c4d829232205df45d08331a53ac5
burnt-toast <nocryingoverburnttoast@gmail.com>
2019-03-23 20:51:31 +0000

    added another way to loop through network

--- monitor.py
+++ monitor.py
@@ -5,8 +5,8 @@ def runScan():
     nm = nmap.PortScanner()
     nm.scan(hosts='192.168.1.*/24', arguments='-n -sP -PE')
     
-    hostlist = ' '.join(nm.all_hosts())
-    nm.scan(hosts=hostlist, arguments='-n -sP -PE')
+    #hostlist = ' '.join(nm.all_hosts())
+    #nm.scan(hosts=hostlist, arguments='-n -sP -PE')
     
     for host in nm.all_hosts():
         print('----------------------------------------------------')
@@ -17,5 +17,21 @@ def runScan():
         except:
             print("Could not identify host")
 
+'''
+Loops through all potential IPs on the local network, but detects inactive devices
+'''
+def runOtherScan():
+    hostList = []
+    for i in range(1, 255):
+        try:
+            host = "192.168.1."+ str(i)
+            result = socket.gethostbyaddr(host)
+            hostList.append((host, result[0]))
+        except:
+            pass
+        
+    print(hostList)
+
 if __name__ == "__main__":
     runScan()
+    #runOtherScan()
